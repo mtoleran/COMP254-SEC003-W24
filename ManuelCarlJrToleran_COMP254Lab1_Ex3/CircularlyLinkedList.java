018aeff30cca9a261b35bc9f8529ca156ca27e15
mtoleran <manuelcarltoleran@gmail.com>
2024-01-27 20:21:21 -0500

    Added Driver class

--- ManuelCarlJrToleran_COMP254Lab1_Ex3/CircularlyLinkedList.java
+++ ManuelCarlJrToleran_COMP254Lab1_Ex3/CircularlyLinkedList.java
@@ -153,30 +153,5 @@ public class CircularlyLinkedList<E> {
     sb.append(")");
     return sb.toString();
   }
-  
-//main method
-  public static void main(String[] args)
-  {
-      System.out.println("\n--Clone a populated list--");
-	  CircularlyLinkedList<String> circularList = new CircularlyLinkedList<String>();
-      circularList.addFirst("RBC");
-      circularList.addLast("TD");
-      circularList.addLast("BMO");
-      circularList.addLast("Scotiabank");
-      circularList.addLast("CIBC");
-      circularList.addLast("NBC");
 
-	  System.out.println("Original");
-	  System.out.println(circularList);
-
-      System.out.println("\nCopy");
-      CircularlyLinkedList<String> copy = circularList.clone();
-      System.out.println(copy);
-
-    //clone an empty list
-    System.out.println("\n--Clone an empty list--");
-    CircularlyLinkedList<String> circularList2 = new CircularlyLinkedList<String>();
-    CircularlyLinkedList<String> copy2 = circularList2.clone();
-    System.out.println(copy2);
-  }
 }
