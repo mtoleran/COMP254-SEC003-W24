66f5530a1395d457cc973d37514a7f8212566e01
mtoleran <manuelcarltoleran@gmail.com>
2024-01-27 19:44:19 -0500

    Updated SinglyLinkedList file and added Node file

--- ManuelCarlJrToleran_COMP254Lab1_Ex2/SinglyLinkedList.java
+++ ManuelCarlJrToleran_COMP254Lab1_Ex2/SinglyLinkedList.java
@@ -35,46 +35,6 @@ public class SinglyLinkedList<E> implements Cloneable {
    * element and to the subsequent node in the list (or null if this
    * is the last node).
    */
-  private static class Node<E> {
-
-    /** The element stored at this node */
-    private E element;            // reference to the element stored at this node
-
-    /** A reference to the subsequent node in the list */
-    private Node<E> next;         // reference to the subsequent node in the list
-
-    /**
-     * Creates a node with the given element and next node.
-     *
-     * @param e  the element to be stored
-     * @param n  reference to a node that should follow the new node
-     */
-    public Node(E e, Node<E> n) {
-      element = e;
-      next = n;
-    }
-
-    // Accessor methods
-    /**
-     * Returns the element stored at the node.
-     * @return the element stored at the node
-     */
-    public E getElement() { return element; }
-
-    /**
-     * Returns the node that follows this one (or null if no such node).
-     * @return the following node
-     */
-    public Node<E> getNext() { return next; }
-
-    // Modifier methods
-    /**
-     * Sets the node's next reference to point to Node n.
-     * @param n    the node that should follow this one
-     */
-    public void setNext(Node<E> n) { next = n; }
-    
-  } //----------- end of nested Node class -----------
 
   // instance variables of the SinglyLinkedList
   /** The head node of the list */
